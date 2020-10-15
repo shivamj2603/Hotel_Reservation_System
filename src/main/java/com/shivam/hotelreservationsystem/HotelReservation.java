6b90fcb94f51dec33b2132e1a9dc464e6fcd76a6
shivamj2603 <shivamj2603@gmail.com>
2020-10-16 01:21:19 +0530

    Modified:findCheapest Hotel function to incorporate variable rates

--- src/main/java/com/shivam/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/shivam/hotelreservationsystem/HotelReservation.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 import java.time.temporal.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class HotelReservation {
@@ -15,13 +16,16 @@ public class HotelReservation {
 	 */
 	public boolean addHotel(Scanner input) {
 		String hotelName = "";
-		int rate;
+		int weekDayRate, weekEndRate;
 		System.out.println("Enter the Hotel Name:");
 		hotelName = input.nextLine();
-		System.out.println("Enter the Rates For Regular Customers(in $)");
-		rate = input.nextInt();
+		System.out.println("Enter the WeekDay Rates For Regular Customers(in $)");
+		weekDayRate = input.nextInt();
 		input.nextLine();
-		Hotel hotel = new Hotel(hotelName, rate, "Regular");
+		System.out.println("Enter the Weekend Rates For Regular Customers(in $)");
+		weekEndRate = input.nextInt();
+		input.nextLine();
+		Hotel hotel = new Hotel(hotelName, weekDayRate, weekEndRate, "Regular");
 		boolean added = hotelList.add(hotel);
 		return added;
 	}
@@ -35,41 +39,68 @@ public class HotelReservation {
 	public void setHotelList(ArrayList<Hotel> hotelList) {
 		this.hotelList = hotelList;
 	}
-	public Hotel findCheapestHotel(Scanner input) {
+	/**
+	 * Usecase 4
+	 * Modification of the function to take into account different rates for weekend and weekdays
+	 * @param input
+	 * @return
+	 */
+	public ArrayList<Hotel> findCheapestHotel(Scanner input) {
 		LocalDate[] localDate = new LocalDate[2];
 		String inputDate = "";
 		inputDate = input.nextLine();
+		//Split the string to get the dates
 		String[] dates = inputDate.split(",");
 		for(int iteration = 0; iteration<=1 ; iteration++) {
+			//Convert dates to standard format
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy", Locale.ENGLISH);
 			LocalDate date = LocalDate.parse(dates[iteration], formatter);
 			localDate[iteration] = date;
 		}	
-		long totalDays = ChronoUnit.DAYS.between(localDate[0], localDate[1]);
+		//start date
+	    LocalDate start = localDate[0];
+	    //end date
+	    LocalDate end = localDate[1];
+		long totalDays = ChronoUnit.DAYS.between(start, end);
 		totalDays = totalDays + 1;
-		Hotel cheapestHotel = this.findCheapest();
-		int totalRate = cheapestHotel.getRate() * (int)totalDays;
-		System.out.println(cheapestHotel.getHotelName()+ ", Total Rates : $"+totalRate);
-		return cheapestHotel;
+		long totalWeekEndDays = getTotalWeekEndDays(start, end);
+		long totalWeekDays = totalDays - totalWeekEndDays;
+		ArrayList<Hotel> cheapestHotels = this.findCheapest(totalWeekDays, totalWeekEndDays);
+		return cheapestHotels;
 	}
-	public Hotel findCheapest() {
-		int minimum = hotelList.get(0).getRate();
+	public ArrayList<Hotel> findCheapest(long totalWeekDays, long totalWeekEndDays) {
+		int minimum = 0;
+		ArrayList<Hotel> cheapestHotels = new ArrayList<Hotel>();
+		HashMap<Hotel,Integer> hotelMap = new HashMap<Hotel,Integer>();
 		for(Hotel hotel : hotelList) {
-			if(hotel.getRate()<minimum) {
-				minimum = hotel.getRate();
+			int totalRate = hotel.getWeekDayRate() * (int)totalWeekDays + hotel.getWeekEndRate() * (int)totalWeekEndDays;
+			hotelMap.put(hotel, totalRate);
+		}
+	    minimum = Collections.min(hotelMap.values());
+	    for(Map.Entry<Hotel, Integer> entry : hotelMap.entrySet()) {
+	    	if(entry.getValue() == minimum) {
+	    		cheapestHotels.add(entry.getKey());
+	    	}
+	    }
+	    for(Hotel hotel : cheapestHotels) {
+	    System.out.println(hotel.getHotelName() + " Total Rate : " + minimum);
+	    }
+	    return cheapestHotels;
+	}
+	public long getTotalWeekEndDays(LocalDate start, LocalDate end) {
+		long totalWeekEndDays = 0;
+		LocalDate next = start.minusDays(1);
+		//iterate from start date to end date
+		while ((next = next.plusDays(1)).isBefore(end.plusDays(1))) {
+			if(next.getDayOfWeek().toString().equals("SATURDAY") || next.getDayOfWeek().toString().equals("SUNDAY")) {
+				totalWeekEndDays++;
 			}
 		}
-		for(Hotel hotel : hotelList) {
-			if(minimum == hotel.getRate()) {
-				return hotel;
-			}
-		}
-		return null;
+		return totalWeekEndDays;	
 	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to Hotel Reservation Program");
-		HotelReservation reservation = new HotelReservation();
-		reservation.findCheapestHotel(input);
+		input.close();
 	}
 }
--- src/test/java/com/shivam/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/shivam/hotelreservationsystem/HotelReservationTest.java
@@ -1,11 +1,15 @@
 package com.shivam.hotelreservationsystem;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class HotelReservationTest {
+//	/**
+//	 * TestCase 1
+//	 */
 //	@Test
 //	public void whenHotelAdded_toReservationSystem_shouldReturnTrue() {
 //		Scanner input = new Scanner(System.in);
@@ -13,15 +17,36 @@ class HotelReservationTest {
 //		boolean result = reservation.addHotel(input);
 //		assertTrue(result);
 //	}
+//	/**
+//	 * TestCase 4
+//	 * TestCase 2 modified 
+//	 * 
+//	 */
 	@Test
 	public void whenCalled_findCheapestHotelFunction_shouldReturnCheapestHotel() {
 		Scanner input = new Scanner(System.in);
 		HotelReservation reservation = new HotelReservation();
 		reservation.addHotel(input);
 		reservation.addHotel(input);
-		Hotel hotel = new Hotel("LakeWood", 100 ,"Regular");
-		boolean result = hotel.getRate() == reservation.findCheapestHotel(input).getRate() ? true : false;
-		assertTrue(result);
-		
+		reservation.addHotel(input);
+		Hotel hotel = new Hotel("LakeWood", 110, 90,"Regular");
+		ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+		hotelList.add(hotel);
+		assertEquals(hotelList, reservation.findCheapestHotel(input));
 	}
+//	/**
+//	 * TestCase 3
+//	 * 
+//	 */
+//	@Test
+//	public void whenHotelAdded_toReservationSystem_withWeekdayAndWeekendRates_shouldReturnSizeAs3() {
+//		Scanner input = new Scanner(System.in);
+//		HotelReservation reservation = new HotelReservation();
+//		reservation.addHotel(input);
+//		reservation.addHotel(input);
+//		reservation.addHotel(input);
+//		assertEquals(3, reservation.hotelList.size());
+//	}
+//	
+	
 }
