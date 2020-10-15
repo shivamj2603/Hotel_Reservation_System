109ed0aac976b9f88847bd23f83e2b408191cf5b
shivamj2603 <shivamj2603@gmail.com>
2020-10-15 23:20:25 +0530

    Added:Functionality to find cheapest hotels gor regular customers

--- src/main/java/com/shivam/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/shivam/hotelreservationsystem/HotelReservation.java
@@ -1,4 +1,7 @@
 package com.shivam.hotelreservationsystem;
+import java.time.LocalDate;
+import java.time.temporal.*;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.Scanner;
 
@@ -11,7 +14,7 @@ public class HotelReservation {
 	 * @return
 	 */
 	public boolean addHotel(Scanner input) {
-		String hotelName ="";
+		String hotelName = "";
 		int rate;
 		System.out.println("Enter the Hotel Name:");
 		hotelName = input.nextLine();
@@ -32,11 +35,41 @@ public class HotelReservation {
 	public void setHotelList(ArrayList<Hotel> hotelList) {
 		this.hotelList = hotelList;
 	}
+	public Hotel findCheapestHotel(Scanner input) {
+		LocalDate[] localDate = new LocalDate[2];
+		String inputDate = "";
+		inputDate = input.nextLine();
+		String[] dates = inputDate.split(",");
+		for(int iteration = 0; iteration<=1 ; iteration++) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy", Locale.ENGLISH);
+			LocalDate date = LocalDate.parse(dates[iteration], formatter);
+			localDate[iteration] = date;
+		}	
+		long totalDays = ChronoUnit.DAYS.between(localDate[0], localDate[1]);
+		totalDays = totalDays + 1;
+		Hotel cheapestHotel = this.findCheapest();
+		int totalRate = cheapestHotel.getRate() * (int)totalDays;
+		System.out.println(cheapestHotel.getHotelName()+ ", Total Rates : $"+totalRate);
+		return cheapestHotel;
+	}
+	public Hotel findCheapest() {
+		int minimum = hotelList.get(0).getRate();
+		for(Hotel hotel : hotelList) {
+			if(hotel.getRate()<minimum) {
+				minimum = hotel.getRate();
+			}
+		}
+		for(Hotel hotel : hotelList) {
+			if(minimum == hotel.getRate()) {
+				return hotel;
+			}
+		}
+		return null;
+	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to Hotel Reservation Program");
 		HotelReservation reservation = new HotelReservation();
-		reservation.addHotel(input);
-		System.out.println(reservation.getHotelList());
+		reservation.findCheapestHotel(input);
 	}
 }
--- src/test/java/com/shivam/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/shivam/hotelreservationsystem/HotelReservationTest.java
@@ -6,11 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class HotelReservationTest {
+//	@Test
+//	public void whenHotelAdded_toReservationSystem_shouldReturnTrue() {
+//		Scanner input = new Scanner(System.in);
+//		HotelReservation reservation = new HotelReservation();
+//		boolean result = reservation.addHotel(input);
+//		assertTrue(result);
+//	}
 	@Test
-	public void whenHotelAdded_toReservationSystem_shouldReturnTrue() {
+	public void whenCalled_findCheapestHotelFunction_shouldReturnCheapestHotel() {
 		Scanner input = new Scanner(System.in);
 		HotelReservation reservation = new HotelReservation();
-		boolean result = reservation.addHotel(input);
+		reservation.addHotel(input);
+		reservation.addHotel(input);
+		Hotel hotel = new Hotel("LakeWood", 100 ,"Regular");
+		boolean result = hotel.getRate() == reservation.findCheapestHotel(input).getRate() ? true : false;
 		assertTrue(result);
+		
 	}
 }
