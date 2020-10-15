270029c3dd277e0ecdbd2438bafba2d49e57c961
shivamj2603 <shivamj2603@gmail.com>
2020-10-15 19:42:13 +0530

    Hotel Reservation System

---
+++ src/main/java/com/shivam/hotelreservationsystem/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.shivam.hotelreservationsystem;
+
+public class HotelReservation {
+	public static void maib(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
